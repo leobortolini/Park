f8de31325ec0a537c673628e0d2e7ec3393d8094
eduardo-brigato <edubrigato@gmail.com>
2024-09-16 18:53:52 -0300

    fix: disable spring.jpa.open-in-view and update annotation to @RestController

--- src/main/java/br/com/park/payment/controller/PaymentController.java
+++ src/main/java/br/com/park/payment/controller/PaymentController.java
@@ -7,13 +7,12 @@ import br.com.park.payment.service.PaymentService.PaymentReceipt;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/payment")
 @Slf4j
 public class PaymentController {
